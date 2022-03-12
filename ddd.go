26189b0bd876900819dec61da831d401fd826186
Jakezo <cakim21@naver.com>
2022-03-12 23:33:49 +0900

    go start

---
+++ ddd.go
@@ -0,0 +1,13 @@
+package main
+
+import f "fmt"
+
+func main() {
+	name := "jaeik" // 선언과 동시에 변수를 초기화 할 수있다.
+	age := 27
+
+	var title string = "Golang"
+	var number int = 30
+
+	f.Println(name, age, title, number)
+}
